f98b9daae06941d2afae0e132daa676b3ebf7973
EthanGervais <EthanGervais@users.noreply.github.com>
2020-10-25 19:38:14 -0400

    Recieve Info from Args
    Gets the file name, compare type, and sorting type from the arguments.

--- Assignment2Sorting/src/application/AppDriver.java
+++ Assignment2Sorting/src/application/AppDriver.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StreamTokenizer;
+import java.util.Comparator;
 
 import problemdomain.*;
 import utilities.*;
@@ -20,6 +21,7 @@ public class AppDriver {
 	public static void main(String[] args) throws IOException {
 		File path = null;
 		char sortType = 0;
+		Comparator comparator = null;
 
 		for (String arg : args) {
 			if (!arg.startsWith("-") || arg.length() < 3)
@@ -35,31 +37,42 @@ public class AppDriver {
 			case 's':
 				sortType = value.toLowerCase().charAt(0);
 				break;
+			case 't':
+				if (value.toLowerCase().equals("a")) {
+					BaseAreaComparator areaCompare = new BaseAreaComparator();
+					comparator = areaCompare;
+				} else if (value.toLowerCase().equals("v")) {
+					VolumeComparator volCompare = new VolumeComparator();
+					comparator = volCompare;
+				}
+				break;
+			default:
+				System.out.println("Invalid option selected.");
 			}
 
 			System.out.println("Option: " + option + ", value: " + value);
 		}
 
 		FileReader freader = new FileReader(path);
-		
+
 		StreamTokenizer token = new StreamTokenizer(freader);
 		int next = token.nextToken();
-		
+
 		double arrSize = token.nval;
 		next = token.nextToken();
 		Comparable[] shapeArray = new Comparable[(int) arrSize]; // Set size of array to first number in file.
-		
+
 		int i = 0;
 		while (next != StreamTokenizer.TT_EOF) {
 			String shape = token.sval;
 			next = token.nextToken();
-			
+
 			double height = token.nval;
 			next = token.nextToken();
-			
+
 			double otherMeasurement = token.nval;
 			next = token.nextToken();
-			
+
 			if (shape.equalsIgnoreCase("cone")) {
 				Cone cone = new Cone(otherMeasurement, height);
 				shapeArray[i] = cone;
@@ -86,38 +99,13 @@ public class AppDriver {
 			}
 			i++;
 		}
-		
+
 		// Testing the sorting algorithms using the BaseAreaComparator
-		BaseAreaComparator areaCompare = new BaseAreaComparator();
-		VolumeComparator volCompare = new VolumeComparator();
-		
-//		Polygon poly = (Polygon) shapeArray[0];
-//		Polygon poly2 = (Polygon) shapeArray[1];
-//		Polygon poly3 = (Polygon) shapeArray[2];
-//		Polygon poly4 = (Polygon) shapeArray[3];
-//		
-//		System.out.println("\nFirst 4 values before sorting: ");
-//		System.out.println(poly.getArea());
-//		System.out.println(poly2.getArea());
-//		System.out.println(poly3.getArea());
-//		System.out.println(poly4.getArea());
-		
 		long start = System.currentTimeMillis();
-		MyArrays.sort(sortType, shapeArray, areaCompare);
+		MyArrays.sort(sortType, shapeArray, comparator);
 		long end = System.currentTimeMillis();
 		long timeElapsed = end - start;
-		
-//		poly = (Polygon) shapeArray[0];
-//		poly2 = (Polygon) shapeArray[1];
-//		poly3 = (Polygon) shapeArray[2];
-//		poly4 = (Polygon) shapeArray[3];
-//		
-//		System.out.println("\nFirst 4 values after sorting: ");
-//		System.out.println(poly.getArea());
-//		System.out.println(poly2.getArea());
-//		System.out.println(poly3.getArea());
-//		System.out.println(poly4.getArea());
-		
+
 		Polygon lastPoly = (Polygon) shapeArray[(int) (arrSize - 1)];
 		for (int j = 0; j < arrSize - 1; j += 1000) {
 			Polygon nthPoly = (Polygon) shapeArray[j];
@@ -125,8 +113,8 @@ public class AppDriver {
 		}
 		System.out.println(lastPoly.getArea());
 		System.out.println("Time to sort in ms: " + timeElapsed);
-		
-		//System.out.println(shapeArray.length);
+
+		// System.out.println(shapeArray.length);
 
 	}
 
